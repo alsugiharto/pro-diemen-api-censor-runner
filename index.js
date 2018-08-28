8710a93644da4f9a4bc1ba6277379193a0ec2ada
al <>
2018-08-29 00:50:01 +0200

    introducing diemen_config file, multiple point per crontime

--- index.js
+++ index.js
@@ -3,32 +3,41 @@ const express = require('express');
 const CronJob = require('cron').CronJob;
 const request = require('request');
 const nodeCmd = require('node-cmd');
-const data_point = require('./data');
+const point_base = require('./diemen_config.json');
 
-// cronjob time
-new CronJob(data_point.crontime, function() {
+var cron_time = '*/15 * * * * *';
 
-	// run the command and get the callback in data	
-	nodeCmd.get(data_point.cmd, function(err, data, stderr){
+new CronJob(cron_time, function() {
+
+	// looping through diemen config
+	for (var x in point_base){
+
+		data_point = point_base[x];
+
+		// run the command and get the callback in data	
+		nodeCmd.get(data_point.cmd, function(err, data, stderr){
             	
-		// post the request
-		request.post(
-			data_point.url,
-      			{ json: {"id": data_point.id, "point_value": data} },
-      			function (error, response, body) {
-          			if (!error && response.statusCode == 200) {
-					console.log(response.body);
-					console.log('1 point is saved');
-				}else{
-					console.log('ERROR IS HAPPENING');
-					console.log(error);
-					console.log(body);
-					console.log(response);
-				}
-      			}
-  		);
+			// post the request
+			request.post(
+				data_point.url,
+      				{ json: {"id": data_point.point_table_id, "point_value": data} },
+      				function (error, response, body) {
+          				console.log(data_point);
+					if (!error && response.statusCode == 200) {
+						console.log(response.body);
+						console.log('1 point is saved successfully');
+					}else{
+						console.log('ERROR IS HAPPENING');
+						console.log(error);
+						console.log(body);
+						console.log(response);
+					}
+					console.log('====================');
+      				}
+  			);
 
-	});
+		});
+
+	}
 
 }, null, true, 'America/Los_Angeles');
-
