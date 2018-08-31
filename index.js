d3af93cbd80acbd2bafaa87caea3c210d84693cf
al <>
2018-08-31 13:36:13 +0200

    remove loop for now

--- index.js
+++ index.js
@@ -3,43 +3,37 @@ const express = require('express');
 const CronJob = require('cron').CronJob;
 const request = require('request');
 const nodeCmd = require('node-cmd');
-const point_base = require('./diemen_config.json');
+const data_point = require('./diemen_config.json');
 
 var cron_time = '*/15 * * * * *';
 
 new CronJob(cron_time, function() {
-	
-	// looping through diemen config
-	// using for each to make sure all task are done before going to the next loop
-	point_base.forEach(data_point => {
 
-		// run the command and get the callback in data	
-		nodeCmd.get(data_point.cmd, function(err, data, stderr){
+	// run the command and get the callback in data	
+	nodeCmd.get(data_point.cmd, function(err, data, stderr){
 
-			// check for extra function
-			if(data_point.extra_function == 'times100'){
-				data *= 100;
-			}
+		// check for extra function
+		if(data_point.extra_function == 'times100'){
+			data *= 100;
+		}
 
-			// post the request
-			request.post(
-				data_point.url,
-      				{ json: {"id": data_point.point_table_id, "point_value": data} },
-      				function (error, response, body) {
-          				console.log(data_point);
-					if (!error && response.statusCode == 200) {
-						console.log(response.body);
-						console.log('1 point is saved successfully');
-					}else{
-						console.log('ERROR IS HAPPENING');
-						console.log(error);
-						console.log(body);
-					}
-					console.log('====================');
-      				}
-  			);
-
-		});
+		// post the request
+		request.post(
+			data_point.url,
+      			{ json: {"id": data_point.point_table_id, "point_value": data} },
+      			function (error, response, body) {
+          			console.log(data_point);
+				if (!error && response.statusCode == 200) {
+					console.log(response.body);
+					console.log('1 point is saved successfully');
+				}else{
+					console.log('ERROR IS HAPPENING');
+					console.log(error);
+					console.log(body);
+				}
+				console.log('====================');
+      			}
+  		);
 
 	});
 
